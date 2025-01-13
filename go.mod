ecd513cd07ba5ae2f2ffb1980b72b099bfe7e5e0
Emory.Du <iceorangeduxiaocheng@gmail.com>
2025-01-13 17:41:36 +0800

    Added kernel compare load c device

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/emorydu/sdputil
 
 go 1.19
 
-require github.com/thinkeridea/go-extend v1.3.2
+require (
+	github.com/coreos/go-iptables v0.8.0
+	github.com/hashicorp/go-version v1.7.0
+	github.com/thinkeridea/go-extend v1.3.2
+)
--- main/main.go
+++ main/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/coreos/go-iptables/iptables"
 	"github.com/emorydu/sdputil"
+	"github.com/hashicorp/go-version"
 	"github.com/thinkeridea/go-extend/exnet"
+	"io"
 	"os"
+	"os/exec"
 	"strings"
 	"syscall"
 	"unsafe"
@@ -47,15 +52,164 @@ type Rules struct {
 //
 //}
 
+func RunIptablesCommand() error {
+	ipt, err := iptables.New()
+	if err != nil {
+		return fmt.Errorf("创建iptables实例失败: %v", err)
+	}
+
+	exists, err := ipt.Exists("filter", "FORWARD", "-m", "state", "--state", "INVALID,NEW,RELATED,ESTABLISHED", "-j", "ACCEPT")
+	if err != nil {
+		return fmt.Errorf("执行iptables命令失败: %v", err)
+	}
+
+	if exists == false {
+		err = ipt.Insert("filter", "FORWARD", 1, "-m", "state", "--state", "INVALID,NEW,RELATED,ESTABLISHED", "-j", "ACCEPT")
+		if err != nil {
+			return fmt.Errorf("添加默认iptables规则失败: %v", err)
+		}
+
+		ExecCommand("service iptables save")
+
+		exist, err := ipt.Exists("filter", "FORWARD", "-m", "state", "--state", "INVALID,NEW,RELATED,ESTABLISHED", "-j", "ACCEPT")
+		if err != nil {
+			return fmt.Errorf("执行iptables命令失败: %v", err)
+		}
+
+		if !exist {
+			return fmt.Errorf("add default iptables rules faield")
+		}
+
+	}
+
+	return nil
+}
+
+func ExecCommand(params string) (result string, err error) {
+	cmd := exec.Command("/bin/sh", "-c", params)
+	//StdoutPipe方法返回一个在命令Start后与命令标准输出关联的管道。Wait方法获知命令结束后会关闭这个管道，一般不需要显式的关闭该管道。
+	stdout, err := cmd.StdoutPipe()
+	defer stdout.Close()
+	if err != nil {
+		return "", err
+	}
+
+	cmd.Start()
+	//创建一个流来读取管道内内容，这里逻辑是通过一行一行的读取的
+	reader := bufio.NewReader(stdout)
+	sb := strings.Builder{}
+	//实时循环读取输出流中的一行内容
+	for {
+		line, err2 := reader.ReadString('\n')
+		if err2 != nil || io.EOF == err2 {
+			break
+		}
+		v := strings.ReplaceAll(line, "\n", "")
+		sb.WriteString(v)
+		sb.WriteString(";")
+	}
+	//阻塞直到该命令执行完成，该命令必须是被Start方法开始执行的
+	cmd.Wait()
+
+	res := sb.String()
+	sz := len(res)
+
+	if sz > 0 && res[sz-1] == ';' {
+		res = res[:sz-1]
+	}
+
+	return res, nil
+}
+
+func compareKernel() (bool, error) {
+	cmd := exec.Command("uname", "-r")
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return false, err
+	}
+	ver := strings.Split(string(output), "-")[0]
+
+	v1, _ := version.NewVersion(ver)
+	v2, _ := version.NewVersion("4.19")
+	if v1.GreaterThanOrEqual(v2) {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func CheckSDPMiddlewares() error {
+	ok, err := compareKernel()
+	if err != nil {
+		return fmt.Errorf("compare sem version failed: %v", err)
+	}
+	if !ok {
+		res, err := ExecCommand("lsmod|awk {'print $1'}|grep nf_conntrack")
+		if err != nil {
+			return fmt.Errorf("chek nf_conntrace module load failed: %v", err)
+		}
+
+		if strings.Index(res, "nf_conntrack_ipv4") == -1 {
+			ExecCommand("modprobe ip_conntrack")
+
+			result, err := ExecCommand("lsmod|awk {'print $1'}|grep nf_conntrack")
+			if err != nil {
+				return fmt.Errorf("check nf_conntrack module load failed: %v", err)
+			}
+			if strings.Index(result, "nf_conntrack") == -1 {
+				return fmt.Errorf("ip_conntrack module load failed")
+			}
+		}
+
+		if strings.Index(res, "nf_conntrack_ipv6") == -1 {
+			ExecCommand("modprobe nf_conntrack_ipv6")
+
+			result, err := ExecCommand("lsmod|awk {'print $1'}|grep nf_conntrack")
+			if err != nil {
+				return fmt.Errorf("checknf_conntrack module load failed: %v", err)
+			}
+			if strings.Index(result, "nf_conntrack_ipv6") == -1 {
+				return fmt.Errorf("ip_conntrack module load failed")
+			}
+		}
+	} else {
+		ExecCommand("modprobe nf_conntrack")
+		ExecCommand("modprobe ip_conntrack")
+
+		result, err := ExecCommand("lsmod|awk {'print $1'}|grep nf_conntrack")
+		if err != nil {
+			return fmt.Errorf("check nf_conntrack module load faield: %v", err)
+		}
+		if strings.Index(result, "nf_conntrack") == -1 {
+			return fmt.Errorf("nf_conntrack module load failed")
+		}
+	}
+
+	return nil
+}
+
 func main() {
+
 	item = flag.String("items", "", "handle rule item")
 	op = flag.String("op", "", "action (e.g: add/del/show/clear)")
 
 	flag.Parse()
 
+	var err error
+	err = RunIptablesCommand()
+	if err == nil {
+		err = CheckSDPMiddlewares()
+	}
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
 	bd, err := sdputil.Init(nil)
 	if err != nil {
-		fmt.Fprint(os.Stderr, 1)
+		fmt.Fprintln(os.Stderr, 1)
+		return
 	}
 	defer bd.Close()
 
